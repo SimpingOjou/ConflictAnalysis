b4b7ef3aa31d878ed811b4314ddb7a139649052e
SimpingOjou <daregox@gmail.com>
2024-10-29 14:19:18 +0100

    compressed RT comparison into a function

--- Scripts/Analyzer.m
+++ Scripts/Analyzer.m
@@ -8,32 +8,8 @@ parameters;
 % Compute the data
 [tkeo_movement_envelope, tkeo_vibration_envelope, mv_baseline_th, vb_baseline_th, trial_onset_index, test_type, vb_index, mv_index, vb_fing, mv_fing, vb_onset_indexes, mv_onset_indexes] = computeAll(signal, filter_order_mv, cutoff_low_mv, cutoff_high_mv, sampling, tkeo_window_size, vb_alpha, mv_alpha, no_onset_period_ms, vibration_time_ms, vb_filter_order, vb_cutoff_low, vb_cutoff_high, trial_nbr, trial_segment, box_triallist);
 
-%% Printing the RT values
-total_trials = test_type == box_triallist;
-correct_nbr = length(total_trials(total_trials==1));
-incorrect_nbr = length(total_trials(total_trials==0));
-
-fprintf('\nAccuracy: %d of %d >> %f %%',correct_nbr,length(total_trials),correct_nbr/length(total_trials)*100);
-fprintf('Not sure guesses: %d of %d >> %f %%\n',incorrect_nbr,length(total_trials),incorrect_nbr/length(total_trials)*100);
-fprintf('-------------------------------------------------------------------');
-
-% RT comparison with box data
-box_presstime(box_presstime ~= box_null_value) = box_presstime(box_presstime ~= box_null_value) * 1000 + 200; % add vibration duration delay & s to ms
-box_presstime = round(box_presstime); % convert presstime to seconds
-
-rt_acc_index = mv_index - vb_index;
-rt_acc = t(rt_acc_index) * 1000;
-
-fprintf('\nAverage rt | Accelerometer: %f ms | Box: %f ms', mean(rt_acc), mean(box_presstime(box_presstime ~= box_null_value)));
-fprintf('-------------------------------------------------------------------');
-
-% Divide rt based on test type
-acc_test_type = test_type(test_type ~= 0);
-
-for i = 1:4
-    fprintf('\nAverage rt (test %d) | Accelerometer: %f ms | Box: %f ms', i, mean(rt_acc(acc_test_type == i)), mean(box_presstime(box_presstime ~= box_null_value & box_triallist == i)));
-    fprintf('-------------------------------------------------------------------');
-end
+% Get RT data
+getRTComparison(test_type, box_triallist, box_presstime, box_null_value, mv_index, vb_index, t)
 
 %% TODO
 % get info about the previous type too

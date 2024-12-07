a083e3306ad08eee9c225e3541b6171085b44c6c
Chang Hui Simone Lin <daregox@gmail.com>
2024-12-07 18:09:34 +0000

    Added median and mean ratio

--- Functions/SegmentedFunctions/doSegmentedStatisticalAnalysis.m
+++ Functions/SegmentedFunctions/doSegmentedStatisticalAnalysis.m
@@ -12,16 +12,23 @@ homo_rt = rt_acc(acc_test_type == 3 | acc_test_type == 4);
 
 if p_hetero < 0.05
     fprintf('\nHeterogeneous reaction times are skewed.');
-    middle_hetero_rt = median(hetero_rt);
 else
     fprintf('\nHeterogeneous reaction times are normal.');
-    middle_hetero_rt = mean(hetero_rt);
 end
 
 if p_homo < 0.05
-    fprintf('\nHomogeneous reaction times are skewed.');
-    middle_homo_rt = median(homo_rt);
+    fprintf('\nHomogeneous reaction times are skewed.\n-------------------------------------------------------------------')
 else
-    fprintf('\nHomogeneous reaction times are normal.');
-    middle_homo_rt = mean(homo_rt); 
+    fprintf('\nHomogeneous reaction times are normal.\n-------------------------------------------------------------------');
 end
+
+median_hetero_rt = median(hetero_rt);
+mean_hetero_rt = mean(hetero_rt);
+median_homo_rt = median(homo_rt);
+mean_homo_rt = mean(homo_rt);
+
+ratio_median = median_hetero_rt / median_homo_rt;
+ratio_mean = mean_hetero_rt / mean_homo_rt;
+
+fprintf('\nRatio of medians (hetero/homo): %.3f', ratio_median);
+fprintf('\nRatio of means (hetero/homo): %.3f', ratio_mean);

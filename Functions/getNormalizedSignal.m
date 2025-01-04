8b5414f813bda0bf6b2e46602828c96d4b4aede2
SimpingOjou <daregox@gmail.com>
2025-01-04 22:30:07 +0100

    Added a function to get the magnitude of the signal

---
+++ Functions/getNormalizedSignal.m
@@ -0,0 +1,14 @@
+% Function to get the magnitude of the signal between the 3 channels
+function [output_signal] = getNormalizedSignal(signal)
+    % Filter the signal
+    cutoff_low = 1; 
+    cutoff_high = 300; 
+    filter_order = 1;
+
+    [b, a] = butter(filter_order, [cutoff_low/(sampling/2), cutoff_high/(sampling/2)], 'bandpass');
+
+    signal = filtfilt(b, a , signal);
+    
+    % Calculate the magnitude of the signal
+    output_signal = sqrt(sum(signal.^2, 2));
+end
